ae66ff980eeaa662a463068c19e1dc0255581a17
Alvin Nguyen <stewlandnguyen313@gmail.com>
2025-01-15 23:53:10 +0700

    Create insert_Data.sql

---
+++ Apache-Nifi-1.13.2/Project 2/sql-scripts/insert_Data.sql
@@ -0,0 +1,11 @@
+INSERT INTO YoutubeVideos (video_id, title, description, channel_title, publish_time, thumbnail_url)
+VALUES 
+('ekr2nIex040', 'ROSÉ & Bruno Mars - APT. (Official Music Video)', 'ROSÉ & Bruno Mars - APT. Download/stream: https://rosesarerosie.lnk.to/APTID Order APT. single CD: ...', 'ROSÉ', '2024-10-18T04:00:07', 'https://i.ytimg.com/vi/ekr2nIex040/default.jpg'),
+
+('U0ZoqmyGJo8', 'Top hits 2024 playlist  ~ Trending music 2024 ~ Best songs 2024 updated weekly (Playlist Hits)', 'Top hits 2024 playlist ~ Trending music 2024 ~ Best songs 2024 updated weekly (Playlist Hits) Top hits 2024 playlist ~ Trending ...', 'Melody Chill Mix', '2024-08-12T11:00:26', 'https://i.ytimg.com/vi/U0ZoqmyGJo8/default.jpg'),
+
+('E2L6NxLf3ic', 'TikTok Songs 2025 - TikTok Songs Playlist - Tik Tok Music 2025', 'TikTok Songs 2025 - TikTok Songs Playlist - Tik Tok Music 2025. Welcome to our TikTok Hits Playlist! . Get ready for a mix of ...', 'Cloudland', '2019-12-13T01:02:57', 'https://i.ytimg.com/vi/E2L6NxLf3ic/default.jpg'),
+
+('k0ubS6BhHu4', 'Tenxi, Naykilla & Jemsii - Garam & Madu (Sakit Dadaku) (Official Music Video)', 'Listen to ''Garam & Madu'' by Tenxi, Naykilla and Jemsii now! Pre-save Spotify & Apple Music ...', 'antinrml', '2024-12-20T12:00:06', 'https://i.ytimg.com/vi/k0ubS6BhHu4/default.jpg'),
+
+('rhOAyPKHWEo', 'Al Shami - Doctor [Official Music Video] (2025) / الشامي - دكتور', 'Directed By: Hassan Rhayel Written & Composed By: Al Shami Music Producer: Fouad Jned Artist Management: Music is My Life ...', 'Al Shami - الشامي', '2025-01-13T14:00:08', 'https://i.ytimg.com/vi/rhOAyPKHWEo/default.jpg');
